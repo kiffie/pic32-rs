ecf892ec2ff3d36de950ef38da03ace43845b2f5
Kiffie van Haash <kiffie@users.noreply.github.com>
2019-12-02 13:29:23 +0100

    support additional modules

--- pic32-hal/src/lib.rs
+++ pic32-hal/src/lib.rs
@@ -1,9 +1,19 @@
 //! HAL library for PIC32
 
 #![no_std]
+#![feature(asm)]
 
+#[cfg(feature = "pic32mx1xxfxxxb")]
+pub use pic32mx1xxfxxxb as pac;
+
+#[cfg(feature = "pic32mx4xxfxxxh")]
+pub use pic32mx4xxfxxxh as pac;
+
+#[cfg(feature = "pic32mx274fxxxb")]
+pub use pic32mx274fxxxb as pac;
 
 pub mod uart;
 pub mod i2c;
-
+pub mod cp0timer;
+pub mod clock;
 
