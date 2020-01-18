db8ec0c149c1c6c94a7cb480417d2bf49a775099
Kiffie van Haash <kiffie@users.noreply.github.com>
2020-01-18 23:17:38 +0100

    move direct access to core timer into a separate struct

--- pic32-hal/src/coretimer.rs
+++ pic32-hal/src/coretimer.rs
@@ -1,52 +1,148 @@
-//! # Core timer (MIPS cp0 registers) access and Delay implementation
+//! Access to the MIPS core timer and implementation of the `DelayMs/DelayUs`
+//! trait
+//!
+//! Uses the MIPS CP0 registers "count" and "compare".
 
-use crate::pac::INT; // interrupt controller
-
-use crate::time::Hertz;
 use crate::hal::blocking::delay::{DelayMs, DelayUs};
+use crate::pac::INT; // interrupt controller
+use crate::time::Hertz;
 
-/// Access to the MIPS core timer
-///
-/// Write access to the count register is intentionally not implemented to avoid
-/// conflicts with the `Delay` functions. The `Delay` functions do not write to the
-/// core timer registers.
-///
-/// The `CountDown` timer implementation, however, modifies the compare register
-/// of the core timer so that an IRQ can be triggered on timer expiration.
-pub struct Coretimer {
+use mips_rt::interrupt;
+use mips_rt::interrupt::Mutex;
+
+use core::cell::Cell;
+
+/// Read Count register (CP0 register 9, select 0)
+fn read_count() -> u32 {
+    let mut count;
+    // does not work with MIPS16
+    unsafe { asm!("mfc0 $0, $$9" : "=r"(count) : : : "volatile") };
+    count
+}
+
+/// Delay implementation based on read-only access to the core timer "count"
+/// register
+pub struct Delay {
     ticks_per_us: u32,
 }
 
-impl Coretimer {
+impl Delay {
     pub const fn new(sysclock: Hertz) -> Self {
         let ticks_per_us = sysclock.0 / 1_000_000 / 2;
-        Coretimer{ ticks_per_us }
+        Delay { ticks_per_us }
+    }
+}
+
+impl DelayMs<u32> for Delay {
+    fn delay_ms(&mut self, ms: u32) {
+        self.delay_us(ms * 1_000);
+    }
+}
+
+impl DelayMs<i32> for Delay {
+    fn delay_ms(&mut self, ms: i32) {
+        if ms >= 0 {
+            self.delay_us((ms as u32) * 1000);
+        }
+    }
+}
+
+impl DelayMs<u16> for Delay {
+    fn delay_ms(&mut self, ms: u16) {
+        self.delay_ms(ms as u32);
+    }
+}
+
+impl DelayMs<u8> for Delay {
+    fn delay_ms(&mut self, ms: u8) {
+        self.delay_ms(ms as u32);
+    }
+}
+
+/// Pauses execution for `us` microseconds
+/// Less efficient implementation that can handle long delays but is not so
+/// well-suited for short delays in the order of a few µs.
+impl DelayUs<u32> for Delay {
+    fn delay_us(&mut self, us: u32) {
+        let mut total_ticks = us as u64 * self.ticks_per_us as u64;
+        while total_ticks != 0 {
+            let current_ticks = if total_ticks <= 0xffff_ffffu64 {
+                total_ticks as u32
+            } else {
+                0xffff_ffffu32
+            };
+            let start = read_count();
+            total_ticks -= current_ticks as u64;
+            while read_count().wrapping_sub(start) < current_ticks {}
+        }
+    }
+}
+
+/// Pauses execution for `us` microseconds
+/// A more efficient implementation suitable for short delays
+impl DelayUs<u16> for Delay {
+    fn delay_us(&mut self, us: u16) {
+        // read the count first for most accurate timing
+        let start = read_count();
+        let ticks = us as u32 * self.ticks_per_us;
+        while read_count().wrapping_sub(start) < ticks {}
+    }
+}
+
+impl DelayUs<u8> for Delay {
+    fn delay_us(&mut self, us: u8) {
+        self.delay_us(us as u16)
+    }
+}
+
+/// Direct access to the MIPS core timer (CP0 timer) and related interrupt control
+///
+/// Can write to the "compare" register of MIPS core timer and can thus be
+/// instantiated only once. Write to the "count" register is not implemented to
+/// avoid conflicts with the `Delay` struct.
+pub struct Timer {}
+
+static TIMER: Mutex<Cell<Option<Timer>>> = Mutex::new(Cell::new(Some(Timer {})));
+
+impl Timer {
+    /// Get the `Timer` singleton. Panics if the singleton is not available.
+    pub fn take() -> Self {
+        let timeropt = interrupt::free(|cs| {
+            let cell = TIMER.borrow(cs);
+            cell.take()
+        });
+        timeropt.unwrap()
+    }
+
+    /// Return the `Timer` singleton.
+    pub fn free(self) {
+        interrupt::free(|cs| {
+            let cell = TIMER.borrow(cs);
+            cell.replace(Some(self));
+        });
     }
 
     /// Read Count register (CP0 register 9, select 0)
     pub fn read_count(&self) -> u32 {
-        let mut count;
-        // does not work with MIPS16
-        unsafe { asm!("mfc0 $0, $$9" : "=r"(count) : : : "volatile")};
-        count
+        read_count()
     }
 
     /// Read Compare register (CP0 register 11, select 0)
     pub fn read_compare(&self) -> u32 {
         let mut compare;
         // does not work with MIPS16
-        unsafe { asm!("mfc0 $0, $$11" : "=r"(compare) : : : "volatile")};
+        unsafe { asm!("mfc0 $0, $$11" : "=r"(compare) : : : "volatile") };
         compare
     }
 
     /// Write to Compare register (CPP0 register 11, select 0)
     pub fn write_compare(&self, compare: u32) {
         // does not work with MIPS16
-        unsafe { asm!("mtc0   $0, $$11" : : "r"(compare) : : "volatile"); }
+        unsafe { asm!("mtc0   $0, $$11" : : "r"(compare) : : "volatile") };
     }
 
     /// Enable interrupts
-    pub fn enable_interrupts(&self, int: &INT){
+    pub fn enable_interrupts(&self, int: &INT) {
         int.iec0set.write(|w| w.ctie().bit(true));
     }
 
@@ -59,70 +155,7 @@ impl Coretimer {
 
     /// Set interrupt priority and sub priority. A priority level of 0 is the
     /// lowest priority level and disables the interrupts.
-    pub fn set_interrupt_prio(&self, int: &INT, prio: u8, subprio: u8){
+    pub fn set_interrupt_prio(&self, int: &INT, prio: u8, subprio: u8) {
         int.ipc0.modify(|_, w| unsafe { w.ctip().bits(prio).ctis().bits(subprio) });
     }
 }
-
-impl DelayMs<u32> for Coretimer {
-    fn delay_ms(&mut self, ms: u32) {
-        self.delay_us(ms * 1_000);
-    }
-}
-
-impl DelayMs<i32> for Coretimer {
-    fn delay_ms(&mut self, ms: i32) {
-        if ms >= 0 {
-            self.delay_us((ms as u32) * 1000);
-        }
-    }
-}
-
-impl DelayMs<u16> for Coretimer {
-    fn delay_ms(&mut self, ms: u16) {
-        self.delay_ms(ms as u32);
-    }
-}
-
-impl DelayMs<u8> for Coretimer {
-    fn delay_ms(&mut self, ms: u8) {
-        self.delay_ms(ms as u32);
-    }
-}
-
-/// Pauses execution for `us` microseconds
-/// Less efficient implementation that can handle long delays but is not so
-/// well-suited for short delays in the order of a few µs.
-impl DelayUs<u32> for Coretimer {
-    fn delay_us(&mut self, us: u32) {
-        let mut total_ticks = us as u64 * self.ticks_per_us as u64;
-        while total_ticks != 0 {
-            let current_ticks = if total_ticks <= 0xffff_ffffu64 {
-                total_ticks as u32
-            } else {
-                0xffff_ffffu32
-            };
-            let start = self.read_count();
-            total_ticks -= current_ticks as u64;
-            while self.read_count().wrapping_sub(start) < current_ticks { }
-        }
-    }
-}
-
-/// Pauses execution for `us` microseconds
-/// A more efficient implementation suitable for short delays
-impl DelayUs<u16> for Coretimer {
-    fn delay_us(&mut self, us: u16) {
-        // read the count first for most accurate timing
-        let start = self.read_count();
-        let ticks = us as u32 * self.ticks_per_us;
-        while self.read_count().wrapping_sub(start) < ticks { }
-    }
-}
-
-impl DelayUs<u8> for Coretimer {
-    fn delay_us(&mut self, us: u8) {
-        self.delay_us(us as u16)
-    }
-}
-
