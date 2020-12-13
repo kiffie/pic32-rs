c8d31dadb09c295d3deb3dc203babd95c6b81f78
Stephan <kiffie@users.noreply.github.com>
2020-12-13 23:12:25 +0100

    move address calculation into separate function

--- mips-rt/src/fmt.rs
+++ mips-rt/src/fmt.rs
@@ -2,13 +2,20 @@
 
 use crate::PhysicalAddress;
 
-/// Calculate a physical address
+/// Convert a virtual to a physical address given as `usize` values
 #[inline(never)]
+fn virt_to_phys_usize(virt: usize) -> usize {
+    if virt >= 0x80000000usize {
+        virt & 0x1fff_ffff
+    } else {
+        virt + 0x4000_0000
+    }
+}
+
+/// Calculate a physical address for a raw pointer
 pub fn virt_to_phys<T>(ptr: *mut T) -> PhysicalAddress {
     let virt: usize = ptr as usize;
-    if virt >= 0x80000000usize {
-        PhysicalAddress { addr: virt & 0x1fff_ffff }
-    } else {
-        PhysicalAddress { addr: virt + 0x4000_0000 }
+    PhysicalAddress {
+        addr: virt_to_phys_usize(virt),
     }
 }
