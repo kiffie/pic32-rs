972dc46dbee5561d67cfc002d9296de23c14ed22
Stephan <kiffie@users.noreply.github.com>
2020-12-13 23:29:40 +0100

    replace Channel builder with Ops trait

--- pic32-hal/src/dma.rs
+++ pic32-hal/src/dma.rs
@@ -5,44 +5,6 @@ use crate::pac::{DMAC, DMAC0, DMAC1, DMAC2, DMAC3};
 use enumflags2::BitFlags;
 use mips_rt::PhysicalAddress;
 
-pub struct DmaChannelBuilder {
-    start_event: Option<InterruptSource>,
-    abort_event: Option<InterruptSource>,
-    abort_pattern: Option<u8>,
-    irq_ena: BitFlags<DmaIrq>, // IRQ Enable flags to be written into DCHxINT register
-}
-
-impl DmaChannelBuilder {
-    pub fn new() -> DmaChannelBuilder {
-        DmaChannelBuilder {
-            start_event: None,
-            abort_event: None,
-            abort_pattern: None,
-            irq_ena: BitFlags::empty(),
-        }
-    }
-
-    pub fn start_event(mut self, event: InterruptSource) -> DmaChannelBuilder {
-        self.start_event = Some(event);
-        self
-    }
-
-    pub fn abort_event(mut self, event: InterruptSource) -> DmaChannelBuilder {
-        self.abort_event = Some(event);
-        self
-    }
-
-    pub fn abort_pattern(mut self, pattern: u8) -> DmaChannelBuilder {
-        self.abort_pattern = Some(pattern);
-        self
-    }
-
-    pub fn irq_enable(mut self, irq: BitFlags<DmaIrq>) -> DmaChannelBuilder {
-        self.irq_ena = irq;
-        self
-    }
-}
-
 /// Interrupt flag or enable bits related to a specific DMA channel
 #[derive(BitFlags, Copy, Clone, Debug, PartialEq)]
 #[repr(u8)]
@@ -65,72 +27,144 @@ pub enum DmaIrq {
     CHER = 0x01,
 }
 
+/// DMA Operations
+///
+/// This trait defines operations that can be carried out by a DMAChannel
+pub trait Ops {
+    fn set_source(&self, addr: PhysicalAddress, size: usize);
+
+    fn set_dest(&self, addr: PhysicalAddress, size: usize);
+
+    fn set_cell_size(&self, size: usize);
+
+    fn set_start_event(&self, event: Option<InterruptSource>);
+
+    fn set_abort_event(&self, event: Option<InterruptSource>);
+
+    fn set_abort_pattern(&self, pattern: Option<u8>);
+
+    fn irq_enable(&self, irq: BitFlags<DmaIrq>);
+
+    /// Enable DMA channel
+    ///
+    /// Unsafe because the DMA controller will access the memory blocks
+    /// specified as source and destination without any checks
+    unsafe fn enable(&self, mode: XferMode);
+
+    /// Check if a DMA channel is enabled
+    ///
+    /// Can be used to poll OneShot channels for completion.
+    fn is_enabled(&self) -> bool;
+
+    /// Disable a DMA channel
+    fn disable(&self);
+}
+
 pub struct DmaChannel<D> {
     ch: D,
 }
 
 macro_rules! dma {
     ($Id:ident, $Dmac:ident) => {
-        impl DmaChannelBuilder {
-            pub fn $Id(self, ch: $Dmac) -> DmaChannel<$Dmac> {
-                // make sure that the DMA controller is enabled
+        impl Ops for DmaChannel<$Dmac> {
+            fn set_source(&self, addr: PhysicalAddress, size: usize) {
                 unsafe {
-                    (*DMAC::ptr()).dmaconset.write(|w| w.on().bit(true));
+                    self.ch.ssa.write(|w| w.bits(addr.address() as u32));
+                    self.ch.ssiz.write(|w| w.bits(size as u32));
                 }
-
-                if let Some(event) = self.start_event {
-                    ch.econ
-                        .modify(|_, w| unsafe { w.chsirq().bits(event as u8).sirqen().bit(true) });
-                }
-                if let Some(event) = self.abort_event {
-                    ch.econ
-                        .modify(|_, w| unsafe { w.chairq().bits(event as u8).airqen().bit(true) });
-                }
-                if let Some(pattern) = self.abort_pattern {
-                    ch.dat.write(|w| unsafe { w.dchpdat().bits(pattern) });
-                    ch.econset.write(|w| w.paten().bit(true));
-                }
-                ch.int
-                    .write(|w| unsafe { w.bits((self.irq_ena.bits() as u32) << 16) });
-                //ch.con.write(|w| w.chen().bit(false));
-
-                DmaChannel { ch }
             }
-        }
 
-        impl DmaChannel<$Dmac> {
-            /// Enable DMA channel using raw PhysicalAddress and usize values
-            ///
-            /// Unsafe because the DMA controller will access the memory blocks
-            /// specified as source and destination without any checks
-            pub unsafe fn enable_raw(
-                &self,
-                mode: XferMode,
-                source: PhysicalAddress,
-                source_size: usize,
-                destination: PhysicalAddress,
-                destination_size: usize,
-                cell_size: usize,
-            ) {
-                self.ch.ssa.write(|w| w.bits(source.address() as u32));
-                self.ch.dsa.write(|w| w.bits(destination.address() as u32));
-                self.ch.ssiz.write(|w| w.bits(source_size as u32));
-                self.ch.dsiz.write(|w| w.bits(destination_size as u32));
-                self.ch.csiz.write(|w| w.bits(cell_size as u32));
+            fn set_dest(&self, addr: PhysicalAddress, size: usize) {
+                unsafe {
+                    self.ch.dsa.write(|w| w.bits(addr.address() as u32));
+                    self.ch.dsiz.write(|w| w.bits(size as u32));
+                }
+            }
+
+            fn set_cell_size(&self, size: usize) {
+                unsafe {
+                    self.ch.csiz.write(|w| w.bits(size as u32));
+                }
+            }
+
+            fn set_start_event(&self, event: Option<InterruptSource>) {
+                match event {
+                    Some(e) => {
+                        self.ch
+                            .econ
+                            .modify(|_, w| unsafe { w.chsirq().bits(e as u8).sirqen().bit(true) });
+                    }
+                    None => {
+                        self.ch.econclr.write(|w| w.sirqen().bit(true));
+                    }
+                }
+            }
+
+            fn set_abort_event(&self, event: Option<InterruptSource>) {
+                match event {
+                    Some(e) => {
+                        self.ch
+                            .econ
+                            .modify(|_, w| unsafe { w.chairq().bits(e as u8).airqen().bit(true) });
+                    }
+                    None => {
+                        self.ch.econclr.write(|w| w.airqen().bit(true));
+                    }
+                }
+            }
+
+            fn set_abort_pattern(&self, pattern: Option<u8>) {
+                match pattern {
+                    Some(p) => {
+                        self.ch.dat.write(|w| unsafe { w.dchpdat().bits(p) });
+                        self.ch.econset.write(|w| w.paten().bit(true));
+                    }
+                    None => {
+                        self.ch.econclr.write(|w| w.paten().bit(true));
+                    }
+                }
+            }
+
+            fn irq_enable(&self, irq: BitFlags<DmaIrq>) {
+                self.ch
+                    .int
+                    .write(|w| unsafe { w.bits((irq.bits() as u32) << 16) });
+            }
+
+            unsafe fn enable(&self, mode: XferMode) {
                 match mode {
                     XferMode::OneShot => self.ch.contclr.write(|w| w.chaen().bit(true)),
                     XferMode::Auto => self.ch.contset.write(|w| w.chaen().bit(true)),
                 }
                 self.ch.contset.write(|w| w.chen().bit(true));
             }
+
+            fn is_enabled(&self) -> bool {
+                self.ch.cont.read().chen().bit()
+            }
+
+            fn disable(&self) {
+                self.ch.contclr.write(|w| w.chen().bit(true));
+                while self.ch.cont.read().chbusy().bit() {}
+            }
+        }
+
+        impl DmaChannel<$Dmac> {
+            pub fn $Id(ch: $Dmac) -> Self {
+                // make sure that the DMA controller is enabled
+                unsafe {
+                    (*DMAC::ptr()).dmaconset.write(|w| w.on().bit(true));
+                }
+                DmaChannel { ch }
+            }
         }
     };
 }
 
-dma!(build_channel0, DMAC0);
-dma!(build_channel1, DMAC1);
-dma!(build_channel2, DMAC2);
-dma!(build_channel3, DMAC3);
+dma!(channel0, DMAC0);
+dma!(channel1, DMAC1);
+dma!(channel2, DMAC2);
+dma!(channel3, DMAC3);
 
 /// indicates whether the channel shall be automatically enabled after a block
 /// transfer
