599723bdeb86c5cc589da98e3e6bc5f376d177d2
Stephan <kiffie@users.noreply.github.com>
2021-09-24 14:40:40 +0200

    rename native lib to libmips-rt.a

--- mips-rt/build.rs
+++ mips-rt/build.rs
@@ -1,8 +1,10 @@
 use std::env;
+use std::fs;
+use std::path::PathBuf;
 
 fn main() {
-    let mut libpath = env::var("CARGO_MANIFEST_DIR").unwrap().to_string();
-    libpath.push_str("/native_lib");
-    println!("cargo:rustc-link-search=native={}", libpath);
-    println!("cargo:rustc-link-lib=static=native");
+    let out_dir = &PathBuf::from(env::var("OUT_DIR").unwrap());
+    fs::copy("native_lib/libmips-rt.a", out_dir.join("libmips-rt.a")).unwrap();
+    println!("cargo:rustc-link-search=native={}", out_dir.display());
+    println!("cargo:rustc-link-lib=static=mips-rt");
 }
