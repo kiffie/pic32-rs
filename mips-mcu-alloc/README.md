52789b4f85bc6d18c16530950850bb166979f165
Stephan <kiffie@users.noreply.github.com>
2022-09-26 19:53:05 +0200

    add mips-mcu-alloc

---
+++ mips-mcu-alloc/README.md
@@ -0,0 +1,48 @@
+# mips-mcu-alloc
+
+[![Crates.io](https://img.shields.io/crates/v/mips-mcu-alloc.svg)](https://crates.io/crates/mips-mcu-alloc)
+[![docs.rs](https://img.shields.io/docsrs/mips-mcu-alloc.svg)](https://docs.rs/mips-mcu-alloc)
+
+A heap allocator for PIC32 microcontrollers (based on the `alloc-cortex-m` crate)
+
+The heap is placed at a location determined by the linker and automatically extended
+to fullfil allocation requests. Automatic heap extension fails if the heap would collide
+with the stack.
+
+Example:
+
+```rust
+#![feature(global_allocator)]
+#![feature(alloc_error_handler)]
+
+// Plug in the allocator crate
+extern crate alloc;
+use alloc::Vec;
+use mips_mcu_alloc::MipsMcuHeap;
+
+#[global_allocator]
+static ALLOCATOR: MipsMcuHeap = MipsMcuHeap::empty();
+
+#[entry]
+fn main() -> ! {
+    ALLOCATOR.init();
+    let mut xs = Vec::new();
+    xs.push(1);
+    loop { /* .. */ }
+}
+
+#[alloc_error_handler]
+fn alloc_error(layout: core::alloc::Layout) -> ! {
+    panic!("Cannot allocate heap memory: {:?}", layout);
+}
+```
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
---
+++ mips-mcu-alloc/src/lib.rs
@@ -0,0 +1,150 @@
+//! A heap allocator for microcontollers with MIPS core such as PIC32 controllers
+//!
+//! The heap is placed at a location determined by the linker and automatically extended
+//! to fullfil allocation requests. Automatic heap extension fails if the heap would collide
+//! with the stack.
+//!
+//! # Example
+//!
+//! ```
+//! #![feature(global_allocator)]
+//! #![feature(alloc_error_handler)]
+//!
+//! // Plug in the allocator crate
+//! extern crate alloc;
+//!
+//! use alloc::Vec;
+//! use mips_mcu_alloc::MipsMcuHeap;
+//!
+//! #[global_allocator]
+//! static ALLOCATOR: MipsMcuHeap = MipsMcuHeap::empty();
+//!
+//! entry!(main);
+//!
+//! #[entry]
+//! fn main() -> ! {
+//!     ALLOCATOR.init();
+//!
+//!     let mut xs = Vec::new();
+//!     xs.push(1);
+//!
+//!     loop { /* .. */ }
+//! }
+//!
+//! #[alloc_error_handler]
+//! fn alloc_error(layout: core::alloc::Layout) -> ! {
+//!     panic!("Cannot allocate heap memory: {:?}", layout);
+//! }
+//!
+//! ```
+
+#![no_std]
+#![feature(asm_experimental_arch)]
+
+use core::alloc::{GlobalAlloc, Layout};
+use core::arch::asm;
+use core::cell::RefCell;
+use core::ptr::{self, NonNull};
+
+use linked_list_allocator::Heap;
+use mips_mcu::interrupt::Mutex;
+use mips_rt::heap_start;
+
+/// Heap extension is performed stepwise. This constant defines the size of one extension step.
+const EXTEND_INCREMENT: usize = 1024;
+
+pub struct MipsMcuHeap {
+    heap: Mutex<RefCell<Heap>>,
+}
+
+impl MipsMcuHeap {
+    /// Crate a new UNINITIALIZED heap allocator
+    ///
+    /// You must initialize this heap using the
+    /// [`init`](struct.CortexMHeap.html#method.init) method before using the allocator.
+    pub const fn empty() -> MipsMcuHeap {
+        MipsMcuHeap {
+            heap: Mutex::new(RefCell::new(Heap::empty())),
+        }
+    }
+
+    /// Initialize heap with heap start location from linker and a defined initial size
+    pub fn init(&self) {
+        let bottom = heap_start() as *mut u8;
+        mips_mcu::interrupt::free(|cs| {
+            unsafe {
+                self.heap
+                    .borrow(*cs)
+                    .borrow_mut()
+                    .init(bottom, EXTEND_INCREMENT)
+            };
+        });
+    }
+
+    /// Returns an estimate of the amount of bytes in use.
+    pub fn used(&self) -> usize {
+        mips_mcu::interrupt::free(|cs| self.heap.borrow(*cs).borrow_mut().used())
+    }
+
+    /// Returns an estimate of the amount of bytes available.
+    pub fn free(&self) -> usize {
+        mips_mcu::interrupt::free(|cs| self.heap.borrow(*cs).borrow_mut().free())
+    }
+
+    /// Returns the start (bottom) of the heap
+    pub fn bottom(&self) -> *mut u8 {
+        mips_mcu::interrupt::free(|cs| self.heap.borrow(*cs).borrow_mut().bottom())
+    }
+
+    /// Returns the end (top) of the heap
+    pub fn top(&self) -> *mut u8 {
+        mips_mcu::interrupt::free(|cs| self.heap.borrow(*cs).borrow_mut().top())
+    }
+}
+
+unsafe impl GlobalAlloc for MipsMcuHeap {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        // try to allocate and successively extend by EXTEND_INCREMENT until memory is exhausted
+        loop {
+            if let Ok(p) = mips_mcu::interrupt::free(|cs| {
+                self.heap
+                    .borrow(*cs)
+                    .borrow_mut()
+                    .allocate_first_fit(layout)
+            }) {
+                break p.as_ptr();
+            } else {
+                // this must be a u8 pointer
+                let new_top: *mut u8 =
+                    mips_mcu::interrupt::free(|cs| self.heap.borrow(*cs).borrow_mut().top())
+                        .add(EXTEND_INCREMENT);
+                // avoid collision with stack
+                if new_top < stack_pointer() {
+                    mips_mcu::interrupt::free(|cs| {
+                        self.heap.borrow(*cs).borrow_mut().extend(EXTEND_INCREMENT)
+                    });
+                } else {
+                    break ptr::null_mut();
+                }
+            }
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        mips_mcu::interrupt::free(|cs| {
+            self.heap
+                .borrow(*cs)
+                .borrow_mut()
+                .deallocate(NonNull::new_unchecked(ptr), layout)
+        });
+    }
+}
+
+fn stack_pointer() -> *mut u8 {
+    let sp: *mut u8;
+    unsafe {
+        asm!(".set noat",
+             "move {0}, $29", out(reg) sp);
+    }
+    sp
+}
