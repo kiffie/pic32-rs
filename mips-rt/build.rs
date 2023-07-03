ca94b1ef1bbe005772ff79cdabd9280aa833a26c
Stephan <kiffie@users.noreply.github.com>
2023-07-03 21:27:02 +0200

    add linker script

--- mips-rt/build.rs
+++ mips-rt/build.rs
@@ -5,6 +5,7 @@ use std::path::PathBuf;
 fn main() {
     let out_dir = &PathBuf::from(env::var("OUT_DIR").unwrap());
     fs::copy("native_lib/libmips-rt.a", out_dir.join("libmips-rt.a")).unwrap();
+    fs::copy("link.x", out_dir.join("link.x")).unwrap();
     println!("cargo:rustc-link-search=native={}", out_dir.display());
     println!("cargo:rustc-link-lib=static=mips-rt");
 }
